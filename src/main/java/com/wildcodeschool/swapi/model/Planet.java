eca799e65809073d7247faea1fe1dc4ebf3dd89d
bastienwcs <email>
2019-10-28 07:37:38 +0100

    fix

--- src/main/java/com/wildcodeschool/swapi/model/Planet.java
+++ src/main/java/com/wildcodeschool/swapi/model/Planet.java
@@ -1,6 +1,13 @@
 package com.wildcodeschool.swapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Planet {
 
-    // TODO
+    // TODO : add attributes
+
+    // TODO : add an empty constructor
+
+    // TODO : add getters and setters
+
 }
